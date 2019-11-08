2af6107310405eb5fb530758508ff37ebe67f187
Devin W. Leaman <Devin.Leaman@jbhunt.com>
2019-11-08 07:37:05 -0600

    Added more Cards endpoints

--- src/endpoints/Cards.ts
+++ src/endpoints/Cards.ts
@@ -1,168 +1,100 @@
-import { Badge, Label } from '../utils/shared';
+import { Action, Attachment, Board, Card, CardFields, Card_CREATE, Card_UPDATE, CheckItem, CheckItem_UPDATE, Checklist, CustomField, List, Member, Sticker, Sticker_UPDATE } from '../utils/shared';
 import BaseEndpoint from './BaseEndpoint';
 
 export default class Cards extends BaseEndpoint {
-  async createCard (newCard: NewCardRequest) {
+  async getCard (id: string, queryParams: Object): Promise<Card | { data: Card, headers: Object }> {
+    return this.performRequest('GET', `/cards/${id}`, queryParams);
+  }
+
+  async getCardField(id: string, field: string, queryParams: Object): Promise<CardFields | { data: CardFields, headers: Object }> {
+    return this.performRequest('GET', `/cards/${id}/${field}`, queryParams);
+  }
+
+  async getCardActions(id: string, queryParams: Object): Promise<Action | { data: Action, headers: Object}> {
+    return this.performRequest('GET', `/cards/${id}/actions`, queryParams);
+  }
+  
+  async getCardAttachments(id: string, queryParams: Object): Promise<Attachment[] | { data: Attachment[], headers: Object }> {
+    return this.performRequest('GET', `/cards/${id}/attachments`, queryParams);
+  }
+
+  async getCardAttachment(cardId: string, attachmentId: string, queryParams: Object): Promise<Attachment | { data: Attachment, headers: Object }> {
+    return this.performRequest('GET', `/cards/${cardId}/attachments/${attachmentId}`, queryParams);
+  }
+
+  async getCardBoard(id: string, queryParams: Object): Promise<Board | { data: Board, headers: Board }> {
+    return this.performRequest('GET', `/cards/${id}/board`, queryParams);
+  }
+
+  async getCardCheckItemStates(id: string, queryParams: Object): Promise<CheckItem[] | { data: CheckItem[], headers: Object }> {
+    return this.performRequest('GET', `/cards/${id}/checkItemStates`, queryParams);
+  }
+
+  async getCardChecklists(id: string, queryParams: Object): Promise<Checklist[] | { data: Checklist[], headers: Object }> {
+    return this.performRequest('GET', `/cards/${id}/checklists`, queryParams);
+  }
+
+  async getCardChecklistCheckItem(cardId: string, checkItemId: string, queryParams: Object): Promise<CheckItem | { data: CheckItem, headers: Object }> {
+    return this.performRequest('GET', `/cards/${cardId}/checkItem/${checkItemId}`, queryParams);
+  }
+
+  async getCardCustomFieldItems(id: string, queryParams: Object): Promise<CustomField[] | { data: CustomField[], headers: Object }> {
+    return this.performRequest('GET', `/cards/${id}/customFieldItems`, queryParams);
+  }
+
+  async getCardList(id: string, queryParams: Object): Promise<List | { data: List, headers: Object }> {
+    return this.performRequest('GET', `/cards/${id}/list`, queryParams);
+  }
+
+  async getCardMembers(id: string, queryParams: Object): Promise<Member[] | { data: Member[], headers: Object }> {
+    return this.performRequest('GET', `/cards/${id}/members`, queryParams);
+  }
+
+  async getCardMembersVoted(id: string, queryParams: Object): Promise<Member[] | { data: Member[], headers: Object }> {
+    return this.performRequest('GET', `/cards/${id}/membersVoted`, queryParams);
+  }
+
+  async getCardPluginData(id: string, queryParams: Object): Promise<Object | { data: Object, headers: Object }> {
+    return this.performRequest('GET', `/cards/${id}/pluginData`, queryParams);
+  }
+
+  async getCardStickers(id: string, queryParams: Object): Promise<Sticker[] | { data: Sticker[], headers: Object }> {
+    return this.performRequest('GET', `/cards/${id}/stickers`, queryParams);
+  }
+
+  async getCardSticker(cardId: string, stickerId: string, queryParams: Object): Promise<Sticker | { data: Sticker, headers: Object }> {
+    return this.performRequest('GET', `/cards/${cardId}/stickers/${stickerId}`, queryParams);
+  }
+
+  async updateCard(id: string, updateCard: Card_UPDATE): Promise<Card | { data: Card, headers: Object }> {
+    return this.performRequest('PUT', `/cards/${id}`, updateCard);
+  }
+
+  async updateCardActionComment(cardId: string, actionId: string, text: string, queryParams: Object): Promise<Card | { data: Card, headers: Object }> {
+    return this.performRequest('PUT', `/cards/${cardId}/actions/${actionId}/comments`, { text: text, ...queryParams });
+  }
+
+  async updateCardCheckItem(cardId: string, checkitemId: string, queryParams: CheckItem_UPDATE): Promise<Card | { data: Card, headers: Object }> {
+    return this.performRequest('PUT', `/cards/${cardId}/checkItem/${checkitemId}`, queryParams);
+  }
+
+  async updateCardChecklistCheckItem(cardId: string, checklistId: string, checkItemId: string, queryParams: Object): Promise<Card | { data: Card, headers: Object }> {
+    return this.performRequest('PUT', `/cards/${cardId}/checklist/${checklistId}/checkItem/${checkItemId}`, queryParams);)
+  }
+  
+  async updateCardSticker(cardId: string, stickerId: string, stickerUpdate: Sticker_UPDATE): Promise<Card | { data: Card, headers: Object }> {
+    return this.performRequest('PUT', `/cards/${cardId}/stickers/${stickerId}`, stickerUpdate);
+  }
+  
+  async createCard (newCard: Card_CREATE) {
     return this.performRequest('POST', `/cards`, newCard)
   }
-}
 
-export declare class Card {
-  /** The unique identifier of the card. */
-  id: string;
+  async addCardActionComment(id: string, comment: string, queryParams: Object): Promise<Action | { data: Action, headers: Object }> {
+    return this.performRequest('POST', `/cards/${id}/actions/comments`, { text: comment });
+  }
 
-  /**
-   * Pieces of information about the card that are displayed on the front of it.
-   */
-  badges: Badge;
-
-  checkItemStates: Object[];
-
-  /**
-   * Whether the card is closed (archived). Note: Archived lists and boards do
-   * not cascade archives to cards. A card can have closed:false but be on an
-   * archived board.
-   */
-  closed: boolean;
-
-  /**
-   * The datetime of the last activity on the card. Note: There are activity
-   * that update dateLastActivity that do not create a corresponding action. For
-   * instance, updating the name field of a checklist item on a card does not
-   * create an action but does update the card and board's dateLastActivity.
-   */
-  dateLastActivity: Date;
-
-  /** The description for the card. Up to 16384 chars. */
-  desc: string;
-
-  /** 
-   * If the description has custom emoji, this field will provide the data
-   * necessary to display them.
-   */
-  descData: Object;
-
-  /** The due date on the card, if one exists. */
-  due: Date;
-
-  /** Whether the due date has been marked complete. */
-  dueComplete: boolean;
-
-  /** The id of the attachment selected as the cover image, if one exists. */
-  idAttachmentCover: string;
-
-  /** The ID of the board the card is on. */
-  idBoard: string;
-
-  /** An array of checklist IDs that are on this card. */
-  idChecklists: string[];
-
-  /** An array of label IDs that are on this card. */
-  idLabels: string[];
-
-  /** The ID of the list the card is in. */
-  idList: string;
-
-  /** An array of member IDs that are on this card. */
-  idMembers: string[];
-
-  /** An array of member IDs who have voted on this card. */
-  idMembersVoted: string[];
-
-  /**
-   * Numeric ID for the card on this board. Only unique to the board, and
-   * subject to change as the card moves.
-   */
-  idShort: number;
-
-  /** Array of label objects on this card. */
-  labels: Label[];
-
-  /** 
-   * Whether the card cover image was selected automatically by Trello, or
-   * manually by the user.
-   */
-  manualCoverAttachment: boolean;
-
-  /** The name of the card. */
-  name: string;
-
-  /** Position of the card in the list. */
-  pos: number;
-
-  /** The 8 character shortened ID for the card. */
-  shortLink: string;
-
-  /** URL to the card without the name slug. */
-  shortUrl: string;
-
-  /** Whether this member is subscribed to the card. */
-  subscribed: boolean;
-
-  /** Full URL to the card, with the name slug. */
-  url: string;
-
-  /** Address of card location. */
-  address: string;
-
-  /** Name of card location. */
-  locationName: string;
-
-  /**
-   * Either a comma-separated string in the format latitude, longitude, or an
-   * object containing keys for `latitude` and `longitude` whose values are
-   * numbers between -180 and 180.
-   */
-  coordinates: Object;
-}
-
-declare class NewCardRequest {
-  /** The name for the card. */
-  name: string;
-
-  /** The description for the card. */
-  desc: string;
-
-  /** The position of the new card. `top`, `bottom`, or a positive float. */
-  pos: string;
-
-  /** A due date for the card. */
-  due: string;
-
-  /** Whether or not the due date has been reached. */
-  dueComplete: boolean;
-
-  /** The ID of the list the card should be created in. */
-  idList: string;
-
-  /** Comma-separated list of member IDs to add to the card. */
-  idMembers: string;
-
-  /** Comma-separated list of label IDs to add to the card. */
-  idLabels: string;
-
-  /** A URL starting with `http://` or `https://`. */
-  urlSource: string;
-
-  /** Not sure what this does... */
-  fileSource: string;
-
-  /** The ID of a card to copy into the new card. */
-  idCardSource: string;
-
-  /**
-   * if using `idCardSource` you can specify which properties to copy over.
-   * `all` or comma-separated list of: `attachments,checklists,comments,due,
-   * labels,members,stickers`.
-   */
-  keepFromSource: string;
-
-  /** For use with/by the Map Power-Up. */
-  address: string;
-
-  /** For use with/by the Map Power-Up. */
-  locationName: string;
-
-  /** For use with/by the Map Power-Up. In form of `latitude,longitude`. */
-  coordinates: string;
+  async addCardAttachment(id: string, attachment: Attachment): Promise<Card | { data: Card, headers: Object }> {
+    return this.performRequest('POST', `/cards/${id}/attachments`, attachment);
+  }
 }
